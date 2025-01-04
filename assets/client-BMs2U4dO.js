1b39bf078ed7532ccf1d471f5297415bca2285ed
Andrei Alikov <andrei.alikov@gmail.com>
2025-01-04 19:53:56 +0900

    Updates

--- assets/client-BMs2U4dO.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-DAfSkmQi.js";var t,o=r;t=o.createRoot,o.hydrateRoot;export{t as c};
---
+++ assets/preview-D_-tD24z.js
@@ -0,0 +1,1 @@
+const o={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}}},tags:["autodocs"]};export{o as default};
