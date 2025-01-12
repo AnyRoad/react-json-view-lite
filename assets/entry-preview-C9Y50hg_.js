fd4bf9a9a3ba1018bb45434fe53f28cebfaf4552
Andrei Alikov <andrei.alikov@gmail.com>
2025-01-12 14:32:21 +0900

    Updates

--- assets/entry-preview-C9Y50hg_.js
+++ assets/entry-preview-C9Y50hg_.js
@@ -1,5 +1,5 @@
 const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./react-18-BWhC85Cb.js","./index-DJO9vBfz.js","./index-DJdX7xnk.js"])))=>i.map(i=>d[i]);
-import{_ as tr}from"./iframe-CUhdJFu9.js";import{_ as sr,a as nr,b as L}from"./chunk-XP5HYGXS-BGCqD1aY.js";import{r as Y,R as re}from"./index-DJO9vBfz.js";import{r as ar}from"./index-DJdX7xnk.js";import"../sb-preview/runtime.js";var b={};/**
+import{_ as tr}from"./iframe-D4uO9OXT.js";import{_ as sr,a as nr,b as L}from"./chunk-XP5HYGXS-BGCqD1aY.js";import{r as Y,R as re}from"./index-DJO9vBfz.js";import{r as ar}from"./index-DJdX7xnk.js";import"../sb-preview/runtime.js";var b={};/**
  * @license React
  * react-dom-test-utils.production.min.js
  *
